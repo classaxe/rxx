4e23472877955b05f57d5c460bc822241fadb038
Martin Francis <martin@classaxe.com>
2015-08-18 07:27:29 -0400

    1.0.15: Now includes complete GIT changelog in Admin Info page

--- admin.php
+++ admin.php
@@ -428,6 +428,17 @@ function sys_info()
     $tmp = str_replace(" width=\"600\"", "", $tmp);
     $out = preg_split("/<body>|<\/body>/i", $tmp);
     $phpinfo = $out[1];
+
+    $changelog = explode("\n", `git log master --pretty=format:"%ad %s" --date=short`);
+    foreach($changelog as &$entry) {
+        $bits =     explode(' ', $entry);
+        $date =     array_shift($bits);
+        $version =  trim(array_shift($bits), ':');
+        $details =  implode(' ', $bits);
+        $entry =    $date.'  '.pad($version, 7).' '.$details;
+    }
+    $changelog = implode("\n", $changelog);
+
     return
          "<div id='phpinfo'>\n"
         ."<table border=\"0\" cellpadding=\"3\">\n"
@@ -440,6 +451,7 @@ function sys_info()
         ."<tr><td class=\"e\">system_date</td><td class=\"v\">".system_date."</td></tr>\n"
         ."<tr><td class=\"e\">system_version</td><td class=\"v\">".system_version."</td></tr>\n"
         ."<tr><td class=\"e\">system_revision</td><td class=\"v\">".system_revision."</td></tr>\n"
+        ."<tr><td class=\"e\">Recent Changes</td><td class=\"v\"><pre>".$changelog."</pre></td></tr>\n"
         ."<tr><td class=\"e\">awardsAdminEmail</td><td class=\"v\">".awardsAdminEmail."</td></tr>\n"
         ."<tr><td class=\"e\">awardsAdminName</td><td class=\"v\">".awardsAdminName."</td></tr>\n"
         ."</table>\n"
