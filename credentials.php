3defcbae427886c745785873983dd54a35f37ae0
Martin Francis <martin@classaxe.com>
2018-03-12 06:05:29 -0700

    1.0.88: Now properly handles exclusion of credentials.php from git tracking

--- credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_USER", "rxx");
-define("DB_PASS", "clamp_rxx_777");
-define("DB_DATABASE", "rxx");
-
-define("admin_user", "admin");
-define("admin_password", "777");
-
